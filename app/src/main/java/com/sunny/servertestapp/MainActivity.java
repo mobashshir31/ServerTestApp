26732cca92c891b3aa43028e4b8cf47c2bb14e9a
Mobashshir Jawaid <mobashshir31@gmail.com>
2016-12-14 02:11:13 +0530

    completed Server

--- app/src/main/java/com/sunny/servertestapp/MainActivity.java
+++ app/src/main/java/com/sunny/servertestapp/MainActivity.java
@@ -1,40 +1,129 @@
 package com.sunny.servertestapp;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.net.SocketException;
 import java.util.Enumeration;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView ipInfo;
+    private TextView portInfo;
+    private TextView messageText;
     private ServerSocket serverSocket;
+    static final int serverSocketPort = 8085;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        portInfo = (TextView) findViewById(R.id.portInfo);
+        String str = "Server Port: " + serverSocketPort;
+        portInfo.setText(str);
         ipInfo = (TextView) findViewById(R.id.ipInfo);
+        messageText = (TextView) findViewById(R.id.messageText);
         ipInfo.setText(getLocalIPAddress());
+        Thread serverThread = new Thread(new MyServerSocketRunnable());
+        serverThread.start();
     }
 
-    private class myServerSocketRunnable implements Runnable{
-        static final int serverSocketPort = 8085;
-        @Override
-        public void run() {
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(serverSocket!=null){
             try {
-                serverSocket = new ServerSocket(serverSocketPort);
-                serverSocket.accept();
+                serverSocket.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
     }
+
+    private class MyServerSocketRunnable implements Runnable{
+
+        private int count = 0;
+        @Override
+        public void run() {
+            Socket clientSocket;
+            try {
+                serverSocket = new ServerSocket(serverSocketPort);
+                while(count<15){
+                    clientSocket = serverSocket.accept();
+                    count++;
+                    //Communicate with the newly connected client socket
+                    ServerCommunicateTask serverCommunicateTask = new ServerCommunicateTask(count);
+                    serverCommunicateTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, clientSocket);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private class ServerCommunicateTask extends AsyncTask<Socket,Void,String>{
+        private int clientNumber;
+        ServerCommunicateTask(int clientNumber) {
+            this.clientNumber = clientNumber;
+        }
+
+        @Override
+        protected String doInBackground(Socket... params) {
+            Socket socket = params[0];
+            DataInputStream dataInputStream = null;
+            DataOutputStream dataOutputStream = null;
+            String dispMessage="";
+            try {
+                dataInputStream = new DataInputStream(socket.getInputStream());
+                dataOutputStream = new DataOutputStream(socket.getOutputStream());
+                dispMessage = "Client #" + clientNumber + " is: " + socket.getInetAddress() + ":" + socket.getPort();
+                String messageFromClient = dataInputStream.readUTF();
+                dispMessage += "\nMessage from Client: " + messageFromClient;
+                String replyMessage = "You are Client #" + clientNumber;
+                dataOutputStream.writeUTF(replyMessage);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }finally {
+                if(dataInputStream!=null){
+                    try {
+                        dataInputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if(dataOutputStream!=null){
+                    try {
+                        dataOutputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if(socket!=null){
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return dispMessage;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            messageText.setText(s);
+        }
+    }
+
     private String getLocalIPAddress(){
         String ip = "";
         try{
